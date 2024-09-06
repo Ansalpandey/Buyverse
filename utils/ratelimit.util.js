48397e9cb8cb3e28e78b79f58de7943502757b52
Ansalpandey <pandeyansal77@gmail.com>
2024-09-06 20:45:00 +0530

    refactor: Update rate limiting configuration
    Increase the maximum number of requests per IP to 50 in the rate limiting middleware configuration.

--- utils/ratelimit.util.js
+++ utils/ratelimit.util.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 2, // limit each IP to 50 requests per windowMs
+  max: 50, // limit each IP to 50 requests per windowMs
   message: 'Too many requests from this IP, please try again after an hour',
   statusCode: 429,
   skipSuccessfulRequests: false,
